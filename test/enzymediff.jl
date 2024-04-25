1038c01020a22f657dd98ae8b52d92f3ec746033
Michel Schanen <mschanen@anl.gov>
2024-04-25 11:16:21 -0500

    Add sparse matrix support

--- test/enzymediff.jl
+++ test/enzymediff.jl
@@ -2,63 +2,63 @@ using Enzyme
 import .EnzymeRules: forward, reverse, augmented_primal
 using .EnzymeRules
 
-include("get_div_grad.jl")
-include("utils.jl")
-
 @testset "$solver" for solver = (Krylov.cg, Krylov.gmres, Krylov.bicgstab)
-    function sparse_laplacian(n :: Int=16; FC=Float64)
-    A = get_div_grad(n, n, n)
-    b = ones(n^3)
-    return A, b
-    end
+    @testset "$MT" for MT = (Matrix, SparseMatrixCSC)
+        A, b = sparse_laplacian(4, FC=Float64)
+        A = MT(A)
+        fdm = central_fdm(8, 1);
+        function A_one_one(x)
+            _A = copy(A)
+            _A[1,1] = x
+            solver(_A,b)
+        end
 
-    A, b = sparse_laplacian(4, FC=Float64)
-    denseA = Matrix(A)
-    fdm = central_fdm(8, 1);
-    function A_one_one(x)
-        _A = copy(denseA)
-        _A[1,1] = x
-        solver(_A,b)
-    end
+        function b_one(x)
+            _b = copy(b)
+            _b[1] = x
+            solver(A,_b)
+        end
 
-    function b_one(x)
-        _b = copy(b)
-        _b[1] = x
-        solver(denseA,_b)
-    end
+        fda = FiniteDifferences.jacobian(fdm, a -> A_one_one(a)[1], copy(A[1,1]))
+        fdb = FiniteDifferences.jacobian(fdm, a -> b_one(a)[1], copy(b[1]))
+        fd =fda[1] + fdb[1]
+        # Test forward
+        function duplicate(A::SparseMatrixCSC)
+            dA = copy(A)
+            fill!(dA.nzval, zero(eltype(A)))
+            return dA
+        end
+        duplicate(A::Matrix) = zeros(size(A))
 
-    fda = FiniteDifferences.jacobian(fdm, a -> A_one_one(a)[1], copy(denseA[1,1]))
-    fdb = FiniteDifferences.jacobian(fdm, a -> b_one(a)[1], copy(b[1]))
-    fd =fda[1] + fdb[1]
-    # Test forward
-    ddA = Duplicated(denseA, zeros(size(denseA)))
-    ddb = Duplicated(b, zeros(length(b)))
-    ddA.dval[1,1] = 1.0
-    ddb.dval[1] = 1.0
-    ddx = Enzyme.autodiff(
-        Forward,
-        solver,
-        ddA,
-        ddb
-    )
-    @test isapprox(ddx[1][1], fd, atol=1e-4, rtol=1e-4)
-    # Test reverse
-    function driver!(x, A, b)
-        x .= gmres(A,b)[1]
-        nothing
-    end
-    ddA = Duplicated(denseA, zeros(size(denseA)))
-    ddb = Duplicated(b, zeros(length(b)))
-    ddx = Duplicated(zeros(length(b)), zeros(length(b)))
-    ddx.dval[1] = 1.0
-    Enzyme.autodiff(
-        Reverse,
-        driver!,
-        ddx,
-        ddA,
-        ddb
-    )
+        dA = Duplicated(A, duplicate(A))
+        db = Duplicated(b, zeros(length(b)))
+        dA.dval[1,1] = 1.0
+        db.dval[1] = 1.0
+        dx = Enzyme.autodiff(
+            Forward,
+            solver,
+            dA,
+            db
+        )
+        @test isapprox(dx[1][1], fd, atol=1e-4, rtol=1e-4)
+        # Test reverse
+        function driver!(x, A, b)
+            x .= gmres(A,b)[1]
+            nothing
+        end
+        dA = Duplicated(A, duplicate(A))
+        db = Duplicated(b, zeros(length(b)))
+        dx = Duplicated(zeros(length(b)), zeros(length(b)))
+        dx.dval[1] = 1.0
+        Enzyme.autodiff(
+            Reverse,
+            driver!,
+            dx,
+            dA,
+            db
+        )
 
-    @test isapprox(ddb.dval[1], fdb[1][1], atol=1e-4, rtol=1e-4)
-    @test isapprox(ddA.dval[1,1], fda[1][1], atol=1e-4, rtol=1e-4)
+        @test isapprox(db.dval[1], fdb[1][1], atol=1e-4, rtol=1e-4)
+        @test isapprox(dA.dval[1,1], fda[1][1], atol=1e-4, rtol=1e-4)
+    end
 end
--- test/forwarddiff.jl
+++ test/forwarddiff.jl
@@ -1,12 +1,3 @@
-# Sparse Laplacian.
-include("get_div_grad.jl")
-include("utils.jl")
-solver = Krylov.cg
-function sparse_laplacian(n :: Int=16; FC=Float64)
-  A = get_div_grad(n, n, n)
-  b = ones(n^3)
-  return A, b
-end
 A, b = sparse_laplacian(4, FC=Float64)
 @testset "$solver" for solver = (Krylov.cg, Krylov.gmres, Krylov.bicgstab)
     x, stats = solver(A,b; atol=atol, rtol=rtol)
